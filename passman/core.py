eeed1e82329751f53f157bc0662f347d2f1ff444
Nima Abdollahipour <nima.abdollahi.a@gmail.com>
2023-04-01 12:21:34 -0700

    alphanumeric password

--- passman/core.py
+++ passman/core.py
@@ -21,3 +21,16 @@ def generate_numeric(length):
 def generate_special(length):
     password = [chr(int(random.random()*94)+33) for i in range(length)]
     return ''.join(password)
+
+
+def generate_alphanum(length):
+    password = ''
+    for i in range(length):
+        char_type = random.randint(1,3)
+        if char_type == 1:
+            password += str(random.randint(0,9))
+        elif char_type ==2:
+            password += chr(random.randint(0,25)+ord('a'))
+        elif char_type ==3:
+            password += chr(random.randint(0,25)+ord('A'))
+    return password
